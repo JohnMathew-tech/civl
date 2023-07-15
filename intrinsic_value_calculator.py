49442480ff93abb73c605a2b1438878854ab8fa0
JohnMathew-tech <73165651+JohnMathew-tech@users.noreply.github.com>
2023-07-15 12:55:23 +0530

    Update intrinsic_value_calculator.py

--- intrinsic_value_calculator.py
+++ intrinsic_value_calculator.py
@@ -150,7 +150,7 @@ hide_streamlit_style = """
             div.stActionButton {
                 visibility: hidden;
             }
-            a.viewerBadge_container_rstak.styles_viewerBadge_CvC9N {
+            a.viewerBadge_link_qRIco {
                 visibility: hidden;
             }
 
