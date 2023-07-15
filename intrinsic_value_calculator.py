822094b49dbfab821aa350c82b9d184990569394
JohnMathew-tech <73165651+JohnMathew-tech@users.noreply.github.com>
2023-07-15 13:54:45 +0530

    Update intrinsic_value_calculator.py

--- intrinsic_value_calculator.py
+++ intrinsic_value_calculator.py
@@ -133,6 +133,15 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            footer:after {
+                content:'goodbye'; 
+	            visibility: visible;
+            	display: block;
+	            position: relative;
+	            #background-color: tomato;
+	            padding: 5px;
+	            top: 2px;
+            }
             .stButton > button {
                 color: black;
                 background: gray;
